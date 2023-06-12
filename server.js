f8be0bc841e850be7a5d855d09c00e25affd2914
maryam <maalnajjem@gmail.com>
2023-06-12 02:24:47 -0500

    my css

--- server.js
+++ server.js
@@ -36,18 +36,18 @@ app.use(function(req,res, next){
 
 
 //import our Routes
-const profileRoute = require('./routes/profile')           
+// const profileRoute = require('./routes/profile')           
 const authRoute = require('./routes/auth')
-const bookingRoute = require('./routes/booking')
+// const bookingRoute = require('./routes/booking')
 const indexRoute = require('./routes/index')
 
 
 
  
 //Mount our Routes
-app.use('/', profileRoute)               
+// app.use('/', profileRoute)               
 app.use('/', authRoute)               
-app.use('/', bookingRoute)                 
+// app.use('/', bookingRoute)                 
 app.use('/', indexRoute)               
 
 
